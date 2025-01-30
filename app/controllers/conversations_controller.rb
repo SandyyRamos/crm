08f12b71e013d59101cfd1a5b99661c301f789f4
Sandra Alvarez <sandra.alvarez.ramos18@gmail.com>
2025-01-29 23:25:09 -0600

    cambios en el backend

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,19 +7,18 @@ class ConversationsController < ApplicationController
     # @conversation = Conversation.new(conversations_params)
     # @conversation.lead_id = @lead.id
     if @conversation.save
-      @lead.conversation += 1
-      @lead.save
+      @lead.update_column(:conversation, @lead.conversations.count) # Actualiza el campo 'conversation' con el nuevo número de conversaciones
       redirect_to @lead, notice: 'La conversación fue agregada exitosamente.'
     else
       @conversations = @lead.conversations
       render "leads/show", status: :unprocessable_entity
     end
   end
-end
 
-
-private
+  private
 
 def conversation_params
   params.require(:conversation).permit(:detail)
+end
+
 end
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -4,7 +4,5 @@ class Lead < ApplicationRecord
 
   has_many :conversations, dependent: :destroy
   validates :email, :name, :phone, :status, :add_by, presence: true
-  validates :conversation, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
 end
-
